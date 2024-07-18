5fcfc06fbbd708e88ea15c5e09c9ca725d0f8636
1amSumit <sj779619@gmail.com>
2024-07-18 19:39:35 +0530

    react native setup with expo file based routing

---
+++ app/app/_layout.jsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
