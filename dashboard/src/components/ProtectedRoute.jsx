e00d8fb4652a75adc05be15460dda45a07a3dfce
Vachan MN <vachanmn123@gmail.com>
2024-07-29 19:26:20 +0530

    Fix Login redirect bug

--- dashboard/src/components/ProtectedRoute.jsx
+++ dashboard/src/components/ProtectedRoute.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import AuthContext from "../context/AuthContext";
 
 export default function ProtectedRoute({ element: Component, ...rest }) {
+  const pathname = useLocation().pathname;
   const { authToken } = useContext(AuthContext);
 
-  return authToken ? <Component {...rest} /> : <Navigate to="/login" />;
+  return authToken ? (
+    <Component {...rest} />
+  ) : (
+    <Navigate to={`/login?next=${pathname}`} />
+  );
 }
 
 ProtectedRoute.propTypes = {
